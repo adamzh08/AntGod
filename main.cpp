f87bd45e5459c147502432a3063743b69119f9f1
Adam Zhakenov <adam.zhakenov@gmail.com>
2025-06-05 20:40:29 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,40 @@
+#include <iostream>
+
+#include "raylib.h"
+#include "raymath.h"
+
+#define SCREEN_WIDTH 800
+#define SCREEN_HEIGHT 450
+
+int main() {
+    srand(time(NULL));
+
+    InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "raylib [core] example - basic window");
+
+    SetTargetFPS(60);
+
+    Image ant = LoadImage("assets/ant.png");
+    Texture2D antTexture = LoadTextureFromImage(ant);
+
+    Vector2 mousePos = GetMousePosition();
+    Vector2 antPos = mousePos;
+    float lerpAmount = 0.22f;
+
+    while (!WindowShouldClose()) {
+        BeginDrawing();
+
+        ClearBackground(RAYWHITE);
+
+        mousePos = GetMousePosition();
+        antPos = Vector2Lerp(antPos, mousePos, lerpAmount);
+
+        DrawTextureV(antTexture, antPos, WHITE);
+
+        DrawFPS(GetScreenWidth() - 100, GetScreenHeight() - 25);
+
+        EndDrawing();
+    }
+
+    CloseWindow();
+    return 0;
+}
