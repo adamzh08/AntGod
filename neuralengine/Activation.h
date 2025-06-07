3629d845b4bffeee83e201489a47e78087eb16ea
Adam Zhakenov <adam.zhakenov@gmail.com>
2025-06-07 15:27:38 +0200

    Bug Fix

--- neuralengine/Activation.h
+++ neuralengine/Activation.h
@@ -9,27 +9,27 @@
 #define M_PI 3.14159265358979323846
 
 namespace Activation {
-    float sigmoid(const float x) {
+    inline float sigmoid(const float x) {
         return 1.0 / (1.0 + std::exp(-x));
     }
 
-    float tanh(float x) {
+    inline float tanh(float x) {
         return std::tanh(x);
     }
 
-    float relu(const float x) {
+    inline float relu(const float x) {
         return x > 0 ? x : 0;
     }
 
-    float prelu(const float x, const float alpha) {
+    inline float prelu(const float x, const float alpha) {
         return x > 0 ? x : x * alpha;
     }
 
-    float elu(float x, float alpha) {
+    inline float elu(float x, float alpha) {
         return x > 0.0 ? x : alpha * (std::exp(x) - 1.0);
     }
 
-    float gelu(float x) {
+    inline float gelu(float x) {
         return 0.5 * x * (1.0 + std::tanh(std::sqrt(2.0 / M_PI) * (x + 0.044715 * x * x * x)));
     }
 }
